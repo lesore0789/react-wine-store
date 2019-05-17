09251dcc8026e424d754ec38120509b527cf3b23
lesore0789 <lesore0789@gmail.com>
2019-05-17 00:03:25 -0500

    Add seed data

---
+++ src/data.js
@@ -0,0 +1,111 @@
+export const storeProducts = [
+    {
+      id: 1,
+      title: "Estate Brut Cuvée",
+      img: "img/product-1.png",
+      price: 35,
+      company: "Domaine Carneros",
+      info:
+        "Balance requires neither too much, nor too little, acidity. An inimitable flavor is found in barrels. Sit a minute and sum up the flavors in the finish. Research shows that white wines keep lung tissues healthy. A bouquet becomes more complex and nuanced as the wine ages. Though you'll commonly hear grapes referred to as 'fruit,' botanists technically classify grapes as berries since each fruit forms from a single flower. The bung hole is the opening in a cask through which wine can be put in or taken out.",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 2,
+      title: "Pinot Noir Monterey",
+      img: "img/product-2.png",
+      price: 40,
+      company: "Carmel Road",
+      info:
+        "Balance requires neither too much, nor too little, acidity. An inimitable flavor is found in barrels. Sit a minute and sum up the flavors in the finish. Research shows that white wines keep lung tissues healthy. A bouquet becomes more complex and nuanced as the wine ages. Though you'll commonly hear grapes referred to as fruit, botanists technically classify grapes as berries since each fruit forms from a single flower. The bung hole is the opening in a cask through which wine can be put in or taken out. ",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 3,
+      title: "Merlot",
+      img: "img/product-3.png",
+      price: 30,
+      company: "Red Truck",
+      info:
+        "Balance requires neither too much, nor too little, acidity. An inimitable flavor is found in barrels. Sit a minute and sum up the flavors in the finish. Research shows that white wines keep lung tissues healthy. A bouquet becomes more complex and nuanced as the wine ages. Though you'll commonly hear grapes referred to as fruit, botanists technically classify grapes as berries since each fruit forms from a single flower. The bung hole is the opening in a cask through which wine can be put in or taken out. ",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 4,
+      title: "Vintner's Reserve Sauvignon Blanc",
+      img: "img/product-4.png",
+      price: 38,
+      company: "Kendall-Jackson",
+      info:
+        "Balance requires neither too much, nor too little, acidity. An inimitable flavor is found in barrels. Sit a minute and sum up the flavors in the finish. Research shows that white wines keep lung tissues healthy. A bouquet becomes more complex and nuanced as the wine ages. Though you'll commonly hear grapes referred to as fruit, botanists technically classify grapes as berries since each fruit forms from a single flower. The bung hole is the opening in a cask through which wine can be put in or taken out. ",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 5,
+      title: "Organic Natural Red, NV",
+      img: "img/product-5.png",
+      price: 24,
+      company: "Frey Vineyards",
+      info:
+        "Balance requires neither too much, nor too little, acidity. An inimitable flavor is found in barrels. Sit a minute and sum up the flavors in the finish. Research shows that white wines keep lung tissues healthy. A bouquet becomes more complex and nuanced as the wine ages. Though you'll commonly hear grapes referred to as fruit, botanists technically classify grapes as berries since each fruit forms from a single flower. The bung hole is the opening in a cask through which wine can be put in or taken out. ",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 6,
+      title: "Albarino",
+      img: "img/product-6.png",
+      price: 52,
+      company: "Harley Lane Winery",
+      info:
+        "Balance requires neither too much, nor too little, acidity. An inimitable flavor is found in barrels. Sit a minute and sum up the flavors in the finish. Research shows that white wines keep lung tissues healthy. A bouquet becomes more complex and nuanced as the wine ages. Though you'll commonly hear grapes referred to as fruit, botanists technically classify grapes as berries since each fruit forms from a single flower. The bung hole is the opening in a cask through which wine can be put in or taken out. ",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 7,
+      title: "Ancient Vine Cinsault",
+      img: "img/product-7.png",
+      price: 32,
+      company: "Michael David Winery",
+      info:
+        "Balance requires neither too much, nor too little, acidity. An inimitable flavor is found in barrels. Sit a minute and sum up the flavors in the finish. Research shows that white wines keep lung tissues healthy. A bouquet becomes more complex and nuanced as the wine ages. Though you'll commonly hear grapes referred to as fruit, botanists technically classify grapes as berries since each fruit forms from a single flower. The bung hole is the opening in a cask through which wine can be put in or taken out. ",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 8,
+      title: "Winemaker's Select Riesling",
+      img: "img/product-8.png",
+      price: 38,
+      company: "Snoqualmie",
+      info:
+        "Balance requires neither too much, nor too little, acidity. An inimitable flavor is found in barrels. Sit a minute and sum up the flavors in the finish. Research shows that white wines keep lung tissues healthy. A bouquet becomes more complex and nuanced as the wine ages. Though you'll commonly hear grapes referred to as fruit, botanists technically classify grapes as berries since each fruit forms from a single flower. The bung hole is the opening in a cask through which wine can be put in or taken out. ",
+      inCart: false,
+      count: 0,
+      total: 0
+    }
+  ];
+  
+  export const detailProduct = {
+    id: 1,
+    title: "Estate Brut Cuvée",
+    img: "img/product-1.png",
+    price: 10,
+    company: "Domaine Carneros",
+    info:
+      "Balance requires neither too much, nor too little, acidity. An inimitable flavor is found in barrels. Sit a minute and sum up the flavors in the finish. Research shows that white wines keep lung tissues healthy. A bouquet becomes more complex and nuanced as the wine ages. Though you'll commonly hear grapes referred to as fruit, botanists technically classify grapes as berries since each fruit forms from a single flower. The bung hole is the opening in a cask through which wine can be put in or taken out. ",
+    inCart: false,
+    count: 0,
+    total: 0
+  };
