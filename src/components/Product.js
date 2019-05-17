90188412683dd2a3eb029c68d3436ded28c0b665
lesore0789 <lesore0789@gmail.com>
2019-05-17 01:28:04 -0500

    Create all component files and add react router

---
+++ src/components/Product.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+export default class Product extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Hi from Product</h3>
+            </div>
+        )
+    }
+}
+
