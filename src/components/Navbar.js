3f1fd86a7fb4201a501bb6a55e26c9d01624e7fe
lesore0789 <lesore0789@gmail.com>
2019-05-18 03:40:26 -0500

    Fix navbar padding

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,12 +7,12 @@ import {ButtonContainer} from './Button';
 export default class Navbar extends Component {
     render() {
         return (
-            <NavWrapper className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5"> 
+            <NavWrapper className="navbar navbar-expand-sm bg-primary navbar-dark"> 
                 <Link to='/'>
                 <img src={logo} alt="store" className="navbar-brand" />
                 </Link>
                 <ul className="navbar-nav align-items-center">
-                    <li className="nav-item ml-5">
+                    <li className="nav-item ml-4">
                         <Link to="/" className="nav-link">
                             Products
                         </Link>
