793070a4f3b2ceafde32d638b101eba68a857157
lesore0789 <lesore0789@gmail.com>
2019-05-18 23:06:43 -0500

    Style the buttons and the detail page

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,11 +1,55 @@
 import React, {Component} from 'react';
+import {ProductConsumer} from '../context';
+import {Link} from 'react-router-dom';
+import {ButtonContainer} from './Button';
 
 export default class Details extends Component {
     render() {
         return (
-            <div>
-                <h3>Hi from Details</h3>
-            </div>
+            <ProductConsumer>
+                {(value)=>{
+                const {id, company, img, info, price, title, inCart} = value.detailProduct;
+                return (
+                    <div className="container py-5">
+                    {/* title */}
+                    <div className="row">
+                        <div className="col-10 mx-auto text-center text-slanted text-blue my-5">
+                        <h1>{title}</h1>
+                        </div>
+                    </div>
+                    {/* end of title */}
+                    {/* Product Info */}
+                    <div className="row">
+                        <div className="col-10 mx-auto col-md-6 my-3">
+                        <img src={img} className="img-fluid" alt="product image" />
+                        </div>
+                        {/* Product Text */}
+                        <div className="col-10 mx-auto col-md-6 my-3">
+                        <h2>{title}</h2>
+                        <h4 className="text-title-reg text-muted mt-3 mb-2">
+                        Made By: <span>{company}</span>
+                        </h4>
+                        <h4 className="text-blue">
+                        <strong>
+                            price: <span>$</span> {price}
+                        </strong>
+                        </h4>
+                        <p className="font-weight-bold mt-3 mb-0">Some Info About This Bottle:</p>
+                        <p className="text-muted lead">{info}</p>
+                        <div>
+                            <Link to="/">
+                            <ButtonContainer>Back to Products</ButtonContainer>
+                            </Link>
+                            <ButtonContainer cart disabled={inCart ? true : false} onClick={()=>{value.addtoCart(id)}}>
+                                {inCart ? "inCart" : "Add to Cart"}
+                            </ButtonContainer>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                )
+                }}
+            </ProductConsumer>
         )
     }
 }
