33c4ad5ad8c827e4851ff79e2a8ad2073186e87b
lesore0789 <lesore0789@gmail.com>
2019-05-20 02:12:00 -0500

    Configure UI for cart and set up initial functionality

--- src/context.js
+++ src/context.js
@@ -12,6 +12,9 @@ class ProductProvider extends Component {
         cart: [],
         modalOpen: false,
         modalProduct: detailProduct,
+        cartSubTotal: 0,
+        cartTax: 0,
+        cartTotal: 0,
     };
     componentDidMount(){
         this.setProducts();
@@ -59,6 +62,18 @@ class ProductProvider extends Component {
             return {modalOpen: false}
         })
     }
+    increment = (id) =>{
+        console.log('this is increment method')
+    }
+    decrement = (id) =>{
+        console.log('this is decrement method')
+    }
+    removeItem = (id) =>{
+        console.log('item removed')
+    }
+    clearCart = () =>{
+        console.log("cart was cleared")
+    }
     render() {
         return (
             <ProductContext.Provider value={{
@@ -66,7 +81,11 @@ class ProductProvider extends Component {
                 handleDetail: this.handleDetail,
                 addToCart: this.addToCart,
                 openModal: this.openModal,
-                closeModal: this.closeModal
+                closeModal: this.closeModal,
+                increment: this.increment,
+                decrement: this.decrement,
+                removeItem: this.removeItem,
+                clearCart: this.clearCart
             }}>
                 {this.props.children}
             </ProductContext.Provider>
